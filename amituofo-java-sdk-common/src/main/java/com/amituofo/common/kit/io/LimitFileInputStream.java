4a6a57e5a4a261b08e5e6e3f68be351bd7a60aa1
rison <localhost>
2026-07-13 14:51:29 +0800

    update

--- amituofo-java-sdk-common/src/main/java/com/amituofo/common/kit/io/LimitFileInputStream.java
+++ amituofo-java-sdk-common/src/main/java/com/amituofo/common/kit/io/LimitFileInputStream.java
@@ -8,17 +8,13 @@ import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class LimitFileInputStream extends InputStream {
+public class LimitFileInputStream extends FileInputStream {
 	private final long OFFSET; // 读取的偏移量
 	private final long MAX_LENGTH; // 最大读取长度
 	private long readLen;
 
-	private InputStream in;
-	private File file = null;
-	private Path path = null;
-
 	public LimitFileInputStream(File file, long offset, long maxLength) throws IOException {
-		this.file = file;
+		super(file);
 		this.OFFSET = offset;
 		this.MAX_LENGTH = maxLength;
 
@@ -27,11 +23,14 @@ public class LimitFileInputStream extends InputStream {
 			throw new IOException("Max length exceed.");
 		}
 
-		this.reset();
+		if (OFFSET > 0) {
+			getChannel().position(OFFSET);
+			readLen = 0;
+		}
 	}
 
 	public LimitFileInputStream(Path path, long offset, long maxLength) throws IOException {
-		this.path = path;
+		super(path.toFile());
 		this.OFFSET = offset;
 		this.MAX_LENGTH = maxLength;
 
@@ -40,7 +39,10 @@ public class LimitFileInputStream extends InputStream {
 			throw new IOException("Max length exceed.");
 		}
 
-		this.reset();
+		if (OFFSET > 0) {
+			getChannel().position(OFFSET);
+			readLen = 0;
+		}
 	}
 
 	@Override
@@ -53,13 +55,14 @@ public class LimitFileInputStream extends InputStream {
 		if (len > remaining) { // 如果请求的长度超过剩余可读取字节数，则只读取剩余部分
 			len = (int) remaining;
 		}
-		int bytesRead = in.read(b, off, len); // 调用父类的read方法读取指定长度的字节
+		int bytesRead = super.read(b, off, len); // 调用父类的read方法读取指定长度的字节
 		if (bytesRead != -1) {
 			readLen += bytesRead; // 更新已读取的偏移量
 		}
 		return bytesRead;
 	}
 
+	@Override
 	public int read(byte[] b) throws IOException {
 		return read(b, 0, b.length);
 	}
@@ -69,7 +72,7 @@ public class LimitFileInputStream extends InputStream {
 		if (readLen >= MAX_LENGTH) { // 如果已经读取了最大长度，则返回-1表示结束
 			return -1;
 		}
-		int v = in.read();
+		int v = super.read();
 		if (v != -1) {
 			readLen++;
 		}
@@ -77,62 +80,19 @@ public class LimitFileInputStream extends InputStream {
 	}
 
 	@Override
-	public long skip(long n) throws IOException {
-		return in.skip(OFFSET + n);
-	}
-
-	public int hashCode() {
-		return in.hashCode();
-	}
-
-	public boolean equals(Object obj) {
-		return in.equals(obj);
-	}
-
-	public String toString() {
-		return in.toString();
-	}
-
 	public int available() throws IOException {
-		return (int) (MAX_LENGTH - readLen);
+		long remaining = MAX_LENGTH - readLen;
+		return remaining > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) remaining;
 	}
 
-	public void mark(int readlimit) {
-		in.mark(readlimit);
-	}
-
-	public void close() throws IOException {
-		in.close();
-		file = null;
-		path = null;
-	}
-
+	@Override
 	public void reset() throws IOException {
-		if (this.in != null) {
-			this.in.close();
-		}
-
-		if (path != null) {
-			this.in = Files.newInputStream(path);
-		} else {
-			this.in = new FileInputStream(file);
-		}
 		if (OFFSET > 0) {
-			in.skip(OFFSET); // 跳过指定长度的字节，从offset开始读取
+			super.getChannel().position(OFFSET);
+		} else {
+			super.getChannel().position(0);
 		}
 		readLen = 0;
-	}
-
-	public SeekableByteChannel getChannel() throws IOException {
-		if (path != null) {
-			return Files.newByteChannel(path);
-		} else {
-			return ((FileInputStream) in).getChannel();
-		}
-	}
-
-	public boolean markSupported() {
-		return in.markSupported();
 	}
 
 }
